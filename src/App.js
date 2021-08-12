4942b0c3ee89e87c98c813d226128e7a26cb8ba7
GAALI GAURAV KRISHNA <81814477+Gaurav-Krishna-Gaali@users.noreply.github.com>
2021-08-12 12:02:54 +0530

    bannner component init

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import "./App.css";
 import Row from "./Row";
 import requests from "./requests";
 
+import Banner from "./Banner";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello gaurav!!</h1>
+      {/* navbar */}
+      <Banner />
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
       />
       <Row title="Trending Now " fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated " fetchUrl={requests.fetchTopRated} />
