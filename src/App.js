f7075c60cff330f6b1a9fd89af20432b400635dc
Gaurav-krishna-Galli <gaali.gk19@gmail.com>
2021-08-13 00:41:00 +0530

    Nav css fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
+import React from "react";
 import "./App.css";
 import Row from "./Row";
 import requests from "./requests";
 import Nav from "./Nav";
-
 import Banner from "./Banner";
 
 function App() {
